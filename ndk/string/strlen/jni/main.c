e6cc25bcc8dca10fcb4bb6f3b04e3bbcb96f647c
leleliu008 <792793182@qq.com>
2014-08-14 04:30:42 +0800

    添加strlen函数测试

---
+++ ndk/string/strlen/jni/main.c
@@ -0,0 +1,8 @@
+#include<string.h>
+#include<stdio.h>
+
+int main() {
+	printf("strlen(\"Hello world!\") = %d\n", strlen("Hello world!"));
+	printf("strlen(\"中国人\") = %d\n", strlen("中国人"));
+	return 0;
+}
