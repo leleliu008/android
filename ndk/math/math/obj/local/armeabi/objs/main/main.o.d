ca3dceb6e936cf65341466eab6ccf4ba79e6f360
leleliu008 <792793182@qq.com>
2014-08-14 14:13:23 +0800

    添加数学函数的测试

---
+++ ndk/math/math/obj/local/armeabi/objs/main/main.o.d
@@ -0,0 +1,76 @@
+obj/local/armeabi/objs/main/main.o: jni/main.c \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/math.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/limits.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/asm/page.h
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/math.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/home/leleliu008/bin/android/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/asm/page.h:
