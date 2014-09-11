f6f4cd17603edf3a8d2209271f7faa060890be50
leleliu008 <792793182@qq.com>
2014-09-12 07:03:17 +0800

    添加uil测试

---
+++ AndroidSDK/src/com/leleliu008/uil/AILActivity.java
@@ -0,0 +1,14 @@
+package com.leleliu008.uil;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AILActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+	}
+}
